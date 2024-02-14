139711303ce20cf44386a095234664cc728a40e4
Daniel Long <danielwlong618@gmail.com>
2024-02-13 21:49:04 -0600

    lab done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -15,24 +15,37 @@ spicy_foods = [
         "heat_level": 6,
     },
 ]
+names = []
 
 def get_names(spicy_foods):
-    pass
+    for dict in spicy_foods:
+         names.append(dict['name'])
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [dict for dict in spicy_foods if dict['heat_level'] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for dict in spicy_foods:
+        strings = dict['name'] + " ("+ dict['cuisine'] + ") | Heat Level: " + "🌶" * dict['heat_level']
+        print (strings)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for dict in spicy_foods:
+        if cuisine == dict['cuisine']:
+            return dict 
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for dict in spicy_foods:
+        if dict['heat_level'] > 5:
+            strings = dict['name'] + " ("+ dict['cuisine'] + ") | Heat Level: " + "🌶" * dict['heat_level']
+            print (strings)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total = sum(food['heat_level'] for food in spicy_foods)
+    return total / len(spicy_foods)
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
